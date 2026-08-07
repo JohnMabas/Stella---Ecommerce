5a0e75349d60a97bf333f4f9cba6fb943f859200
mabas <johnmabas1@gmail.com>
2026-08-07 03:11:49 +0100

    added card 1

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,3 +1,6 @@
+import man1 from "/src/assets/man1.png"
+import { FaHeart } from "react-icons/fa";
+
 function Cards(){
     return(
         <>
@@ -13,7 +16,28 @@ function Cards(){
                 </div>
             </div>
 
-            
+            <div>
+
+                <div className="w-64 space-y-2">
+                    <div className="bg-red-200 rounded-2xl">
+                    <img src={man1} alt="" />
+                    <span className=" relative bottom-64 p-1 text-sm bg-blue-400 ">New Arrival</span>
+                    </div>
+
+                    <div className="flex justify-between">
+                        <p className="text-sm">Uniqio</p>
+                        <span className="text-gray-400 "><FaHeart/></span>
+                    </div>
+                    <h4 className="font-bold">Shirt Soft Cotton</h4>
+                    <div className="flex justify-between">
+                    <p className="text-blue-400">SAR 40.00</p>
+                    <p className="text-red-400 text-sm">12 Items left</p>
+
+                    </div>
+                </div>
+            </div>
+
+
         </section>
 
         </>
